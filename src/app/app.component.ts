243d4c55108bcb34b058017a6f73abb61a804825
51lv3r258 <jrp8900@gmail.com>
2020-07-08 14:49:38 -0500

    :fire: Configuracion back button y SplashScreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,64 @@
-import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, IonRouterOutlet, AlertController } from '@ionic/angular';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Plugins } from '@capacitor/core';
+import { Router } from '@angular/router';
+const { SplashScreen, App } = Plugins;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+	selector: 'app-root',
+	templateUrl: 'app.component.html',
+	styleUrls: [ 'app.component.scss' ]
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+	@ViewChild(IonRouterOutlet, { static: false })
+	routerOutlet: IonRouterOutlet;
+	constructor(
+		private router: Router,
+		private platform: Platform,
+		private statusBar: StatusBar,
+		private alertCtrl: AlertController
+	) {
+		this.initializeApp();
+	}
+	async presentAlertConfirm() {
+		const alert = await this.alertCtrl.create({
+			header: 'Cerrar',
+			message: '¿Esta seguro que desea salir?',
+			buttons: [
+				{
+					text: 'Cancelar',
+					role: 'cancel',
+					cssClass: 'secondary'
+				},
+				{
+					text: 'Confirmar',
+					handler: () => {
+						App.exitApp();
+					}
+				}
+			]
+		});
+		await alert.present();
+	}
+	initializeApp() {
+		this.platform.ready().then(async () => {
+			SplashScreen.hide().catch((error) => {
+				console.warn(error);
+			});
+			if ((this.platform.is('android') || this.platform.is('ios')) && this.platform.is('hybrid')) {
+				this.statusBar.styleDefault();
+			}
+			this.platform.backButton.subscribeWithPriority(0, async () => {
+				if (this.router.url === '/home') {
+					this.presentAlertConfirm();
+				} else if (this.routerOutlet && this.routerOutlet.canGoBack()) {
+					// console.log(this.router.url);
+					this.routerOutlet.pop();
+				} else {
+					this.presentAlertConfirm();
+				}
+			});
+		});
+	}
 }
